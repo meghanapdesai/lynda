a148a150c79b86395af637a16978e0325165c8bd
meghanapdesai <meghanadesai@comcast.net>
2016-04-27 16:38:01 -0500

    Hello world with main function.

---
+++ UpRunningPython/helloworld_start.py
@@ -0,0 +1,10 @@
+#
+# Example file for HelloWorld
+# (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
+
+def main():
+  print "hello world!"
+
+# Calling the main function when file is executed as a program.
+if __name__ == "__main__":
+  main()
