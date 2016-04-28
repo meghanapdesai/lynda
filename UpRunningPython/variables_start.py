3b30e63b3dd2d0bfd5384d3995e9044d06031aa6
meghanapdesai <meghanadesai@comcast.net>
2016-04-27 22:03:20 -0500

    added function call to variables_start.py

--- UpRunningPython/variables_start.py
+++ UpRunningPython/variables_start.py
@@ -5,3 +5,21 @@
 # Declare a variable and initialize it
 f = 0;
 print f
+
+# re-declaring the variable works
+f = "abc"
+print f
+
+# ERROR: variables of different types cannot be combined - have to convert
+#print "string type " + 123
+print "string type " + str(123)
+
+# Global vs. local variables in functions
+def someFunction():
+  #global f
+  f = "def"
+  print f
+  
+somefunction()
+print f
+
