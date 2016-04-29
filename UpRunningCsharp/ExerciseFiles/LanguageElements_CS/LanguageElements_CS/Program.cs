3f0bcabfc52ccc79ef18a23c8c46bd39349d361e
meghanapdesai <meghanadesai@comcast.net>
2016-04-29 14:28:14 -0500

    decision structures

--- UpRunningCsharp/ExerciseFiles/LanguageElements_CS/LanguageElements_CS/Program.cs
+++ UpRunningCsharp/ExerciseFiles/LanguageElements_CS/LanguageElements_CS/Program.cs
@@ -15,35 +15,34 @@ namespace LanguageElements_CS
             Console.WriteLine(Concatenate("First ", "Last"));
 
             #region Data Types
-            //// numeric data types
-            //sbyte aSignedByte = 127;
-            //byte aByte = 255;
-            //ushort anUnsignedShort = 65535;
-            //short aShort = 32767;
-            //int anInt = 2147483647;
-            //uint anUnsignedInt = 4294967295;
-            //ulong anUnsignedLong = 18446744073709551615;
-            //long aLong = 9223372036854775807;
-            //float aFloat = 105.5678493F;
-            //double aDouble = 105.567849312873245;
-            //decimal aDecimal = 105.84923797593487492387492374982347987293487M;
+            // numeric data types
+            sbyte aSignedByte = 127;
+            byte aByte = 255;
+            ushort anUnsignedShort = 65535;
+            short aShort = 32767;
+            int anInt = 2147483647;
+            uint anUnsignedInt = 4294967295;
+            ulong anUnsignedLong = 18446744073709551615;
+            long aLong = 9223372036854775807;
+            float aFloat = 105.5678493F;
+            double aDouble = 105.567849312873245;
+            decimal aDecimal = 105.84923797593487492387492374982347987293487M;
 
-            //// character data types
-            //char aChar = 'A';
-            //string aString = "A string of chars";
+            // character data types
+            char aChar = 'A';
+            string aString = "A string of chars";
 
-            //// Boolean Types
-            //bool trueResult = true;
-            //bool falseResult = false;
+            // Boolean Types
+            bool trueResult = true;
+            bool falseResult = false;
 
-
-            // //Output values, .NET types, size of data type and max value
-            //Console.WriteLine("aByte value is {0}, {1}, {2} Byte(s), {3}", aByte, typeof(byte), sizeof(byte), byte.MaxValue);
-            //Console.WriteLine("anInt value is {0}, {1}, {2} Byte(s), {3}", anInt, typeof(int), sizeof(int), int.MaxValue);
-            //Console.WriteLine("aLong value is {0}, {1}, {2} Byte(s), {3}", aLong, typeof(long), sizeof(long), long.MaxValue);
-            //Console.WriteLine("aFloat value is {0}, {1}, {2} Byte(s), {3}", aFloat, typeof(float), sizeof(float), float.MaxValue);
-            //Console.WriteLine("aDouble value is {0}, {1}, {2} Byte(s), {3}", aFloat, typeof(double), sizeof(double), double.MaxValue);
-            //Console.WriteLine("aDecimal value is {0}, {1}, {2} Byte(s), {3}", aDecimal, typeof(decimal), sizeof(decimal), decimal.MaxValue);
+            //Output values, .NET types, size of data type and max value
+            Console.WriteLine("aByte value is {0}, {1}, {2} Byte(s), {3}", aByte, typeof(byte), sizeof(byte), byte.MaxValue);
+            Console.WriteLine("anInt value is {0}, {1}, {2} Byte(s), {3}", anInt, typeof(int), sizeof(int), int.MaxValue);
+            Console.WriteLine("aLong value is {0}, {1}, {2} Byte(s), {3}", aLong, typeof(long), sizeof(long), long.MaxValue);
+            Console.WriteLine("aFloat value is {0}, {1}, {2} Byte(s), {3}", aFloat, typeof(float), sizeof(float), float.MaxValue);
+            Console.WriteLine("aDouble value is {0}, {1}, {2} Byte(s), {3}", aFloat, typeof(double), sizeof(double), double.MaxValue);
+            Console.WriteLine("aDecimal value is {0}, {1}, {2} Byte(s), {3}", aDecimal, typeof(decimal), sizeof(decimal), decimal.MaxValue);
 
             #endregion
 
@@ -83,51 +82,48 @@ namespace LanguageElements_CS
 
             #region Decision Structures
             // if statement
-            //bool result = false;
-            //if (1 == 1)
-            //{
-            //    Console.WriteLine("Result was true");
-            
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Result was false");
-            //}
-
+            bool result = false;
+            if (1 == 1)
+            {
+                Console.WriteLine("Result was true");
+            }
+            else
+            {
+                Console.WriteLine("Result was false");
+            }
 
             // if..else if
-            //int value = 2;
-            //if (value == 0)
-            //{
-            //    Console.WriteLine("Value is 0");
-            //}
-            //else if (value == 1)
-            //{
-            //    Console.WriteLine("Value is not 0");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Value is something else");
-            //}
-
+            int value = 2;
+            if (value == 0)
+            {
+                Console.WriteLine("Value is 0");
+            }
+            else if (value == 1)
+            {
+                Console.WriteLine("Value is not 0");
+            }
+            else
+            {
+                Console.WriteLine("Value is something else");
+            }
 
             // switch statment
-            //int value = 1;
+            int value = 1;
 
-            //switch (value)
-            //{
-            //    case 0:
-            //        Console.WriteLine("Value is 0");
-            //        break;
+            switch (value)
+            {
+                case 0:
+                    Console.WriteLine("Value is 0");
+                    break;
 
-            //    case 1:
-            //        Console.WriteLine("Value is 1");
-            //        break;
+                case 1:
+                    Console.WriteLine("Value is 1");
+                    break;
 
-            //    default:
-            //        Console.WriteLine("Value is something else");
-            //        break;
-            //}
+                default:
+                    Console.WriteLine("Value is something else");
+                    break;
+            }
             #endregion
         }
 
