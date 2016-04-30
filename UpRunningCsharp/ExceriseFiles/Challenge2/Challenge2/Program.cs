6eaf9518c8ae8b8d877bac3e7488c6004a092d97
meghanapdesai <meghanadesai@comcast.net>
2016-04-30 11:42:16 -0500

    solution for Challenge 2.

---
+++ UpRunningCsharp/ExceriseFiles/Challenge2/Challenge2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum2Ints(23,24);
+        }
+
+        static void Sum2Ints(int one, int two) {
+            int sum = one + two;
+            Console.WriteLine(sum);
+        }
+    }
+}
