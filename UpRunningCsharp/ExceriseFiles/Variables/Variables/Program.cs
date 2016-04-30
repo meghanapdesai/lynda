a1d0b033ac4ef4ffae86813e45428fdc34429ac2
meghanapdesai <meghanadesai@comcast.net>
2016-04-30 11:10:20 -0500

    final commit for Variables

--- UpRunningCsharp/ExceriseFiles/Variables/Variables/Program.cs
+++ UpRunningCsharp/ExceriseFiles/Variables/Variables/Program.cs
@@ -8,9 +8,21 @@ namespace Variables
 {
     class Program
     {
+        struct Person
+        {
+            int age;
+            string firstName;
+            string lastName;
+        }
+
         static void Main(string[] args)
         {
+            int age = 0;
 
+            /// Have to declare value for const
+            const int NUM_MONTHS = 12;
+
+            age = 10;   
         }
     }
 }
