0747208d579cf63df08d8d2afca803f12514a17f
meghanapdesai <meghanadesai@comcast.net>
2016-04-30 19:11:39 -0500

    Challenge5 solution

---
+++ UpRunningCsharp/ExerciseFiles/Challenge5/Challenge5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<string,string> mySortList = new SortedList<string, string>();
+
+            mySortList.Add("first", "String1");
+            mySortList.Add("second", "newString");
+            mySortList.Add("third", "Riddhi");
+        }
+    }
+}
