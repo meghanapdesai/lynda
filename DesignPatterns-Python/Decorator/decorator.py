55239410e7adf1bb8b4faa610ef5f5a7d0c1c9d9
meghanapdesai <meghanadesai@comcast.net>
2016-05-03 11:28:31 -0500

    additional changes

--- DesignPatterns-Python/Decorator/decorator.py
+++ DesignPatterns-Python/Decorator/decorator.py
@@ -4,25 +4,26 @@ def make_blink(function):
 	"""Defines the decorator"""
 
 	#This makes the decorator transparent in terms of its name and docstring
-    @wraps(function)
+	@wraps(function)
 
-	#Define the inner function
-    def decorator():
-	    #Grab the return value of the function being decorated
-	    ret = function()
+    #Define the inner function
+	def decorator():
+		#Grab the return value of the function being decorated
+		ret = function()
 
-	    #Add new functionality to the function being decorated
-	    return "<blink>" + ret + </blink>
+		#Add new functionality to the function being decorated
+		return "<blink>" + ret + "</blink>"
+
 	return decorator
 
 #Apply the decorator here!
-
 def hello_world():
-    """Original function! """
-    return "Hello, World!"
+	"""Original function! """
+
+	return "Hello, World!"
 
 #Check the result of decorating
-print hello_world()
+print (hello_world())
 
 #Check if the function name is still the same name of the function being decorated
 
